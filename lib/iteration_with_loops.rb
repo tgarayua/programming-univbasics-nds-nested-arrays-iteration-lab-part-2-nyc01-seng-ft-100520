59aa50950f6f52873ad454d955f40e6016d80d92
Thomas Garayua <tgarayua@gmail.com>
2020-09-23 13:37:55 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ while row_index < src.count do
   element_index = 0 
   lowest_temp_element = 100
   while element_index < src[row_index].count do 
-    if src[row_index][element_index].length > lowest_temp_element.length
+    if src[row_index][element_index].length > lowest_temp_element
       lowest_temp_element = src[row_index][element_index]
     end
     
